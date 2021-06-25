3090d31b2715baa5612d0ed3e07219a637503c07
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-25 23:43:39 +0300

    feat: add auth model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,19 @@
+import { AuthModel } from './auth.model';
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthController } from './auth.controller';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: AuthModel,
+        schemaOptions: {
+          collection: 'Auth',
+        },
+      },
+    ]),
+  ],
 })
 export class AuthModule {}
