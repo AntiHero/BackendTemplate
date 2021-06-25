2abd4c9558cb1f3c54096e72e47e6d439000fdfe
Aleksandr Schemelev <prophreaker@gmail.com>
2021-06-25 23:57:14 +0300

    feat: add product model

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,15 +1,51 @@
-export class ProductModel {
+import { prop } from '@typegoose/typegoose';
+import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Base } from '@typegoose/typegoose/lib/defaultClasses';
+
+
+class ProductCharactersticts {
+  name: string;
+  value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @prop()
   _id: string;
+  
+  @prop()
   image: string;
+  
+  @prop()
   title: string;
+  
+  @prop()
   price: number;
+  
+  @prop()
   oldPrice: number;
+  
+  @prop()
   credit: number;
+  
+  @prop()
   calculatedRating: number;
+  
+  @prop()
   description: string;
+  
+  @prop()
   advantages: string;
+  
+  @prop()
   disAdvantages: string;
+  
+  @prop({ type: () => [String] })
   categories: string[];
+  
+  @prop({ type: () => [String] })
   tags: string[];
-  characteristics: Record<string, string>;
+
+  @prop({ type: () => [ProductCharactersticts], _id: false})
+  characteristics: ProductCharactersticts[];
 }
